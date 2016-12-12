a41b5c08c225896ea15d86070dfb157cc5bce246
David Cramer <dcramer@gmail.com>
2016-12-12 12:59:01 -0800

    Add basic tests

---
+++ test_pytest_responses.py
@@ -0,0 +1,24 @@
+from __future__ import (
+    absolute_import, print_function, division, unicode_literals
+)
+
+import requests
+import responses
+import pytest
+
+from requests.exceptions import ConnectionError
+
+
+@pytest.mark.withoutresponses
+def test_disabled():
+    with pytest.raises(ConnectionError):
+        requests.get('http://responses.invalid')
+
+    assert len(responses.calls) == 0
+
+
+def test_enabled():
+    with pytest.raises(ConnectionError):
+        requests.get('http://responses.invalid')
+
+    assert len(responses.calls) == 1
