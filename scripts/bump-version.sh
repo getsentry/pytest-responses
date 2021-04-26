64c0104cd6f417acebbfd9819098355142f39b86
Chad Whitacre <chadwhitacre@sentry.io>
2021-04-26 13:53:58 -0400

    Sorry, Perl

--- scripts/bump-version.sh
+++ scripts/bump-version.sh
@@ -1,13 +1,7 @@
 #!/bin/bash
 set -eux
 
-if [ "$(uname -s)" != "Linux" ]; then
-    echo "Please use the GitHub Action."
-    exit 1
-fi
-
-SCRIPT_DIR="$( dirname "$0" )"
-cd $SCRIPT_DIR/..
+cd "$(dirname "$0")/.."
 
 OLD_VERSION="${1}"
 NEW_VERSION="${2}"
@@ -16,9 +10,10 @@ echo "Current version: $OLD_VERSION"
 echo "Bumping version: $NEW_VERSION"
 
 function replace() {
-    ! grep "$2" $3
-    perl -i -pe "s/$1/$2/g" $3
-    grep "$2" $3  # verify that replacement was successful
+    ! grep "$2" "$3"
+    sed -e "s/$1/$2/g" "$3" > "$3.tmp"  # -i is non-portable
+    mv "$3.tmp" "$3"
+    grep "$2" "$3"  # verify that replacement was successful
 }
 
-replace "version=\"[0-9.]+\"" "version=\"$NEW_VERSION\"" ./setup.py
+replace "version='[^']*'" "version='$NEW_VERSION'" ./setup.py
